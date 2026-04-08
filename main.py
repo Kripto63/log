0716b0ac88d003e23fdb1e58f23cbdd6da84a7dc
kripto63 <ignatov.fedor.evgenivich@gmail.com>
2026-04-09 01:37:47 +0400

    add test data

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import re
+
 import model.test
 import model.vector
 from create import examination_poof
@@ -5,18 +7,25 @@ from create import examination_poof
 
 if __name__ == '__main__':
     f = []
-    words = 'word text to1 file1 not1 did casel1'
-    words2 = 'word text to file1 not11 to casel1'
 
-    # Создание объекта Hash_word и присваение строки
-    poof = model.test.Hash_word(words)
+    patern = r'(\[\d{4}\-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\]) (INFO|DEBUG): (.*)'
 
-    # в переменную test помещаем масив хэшей
-    test = poof.get_list_hex_word()
-    test2 = model.test.Hash_word(words2).get_list_hex_word()
+    with open('test_data.txt', 'r') as f:
+        file_log = re.findall(patern, f.read())
 
-    examination_poof(test, test2, poof, f)
+    for s in file_log:
+        print(s)
 
-    print(f)
+
+    # # Создание объекта Hash_word и присваение строки
+    # poof = model.test.Hash_word(words)
+    #
+    # # в переменную test помещаем масив хэшей
+    # test = poof.get_list_hex_word()
+    # test2 = model.test.Hash_word(words2).get_list_hex_word()
+    #
+    # examination_poof(test, test2, poof, f)
+    #
+    # print(f)
     
 
