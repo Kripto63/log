2864736c4899ac3e3475cba0546b0b74151ff287
kripto <ignatov.fedor.evgenivich@gmail.com>
2026-04-09 10:33:00 +0400

    Auto stash before rebase of "refs/heads/main"

--- create.py
+++ create.py
@@ -22,24 +22,26 @@ def examination_poof(test, test2, poof, f):
         f.append({poof.get_hex(): poof})
 
 def comparison_poof(test, test2, poof) -> list:
+    # если количество слов в предлоение отличается, то это разные предлоения
     if (len(test) == len(test2)):
-        list_hex_word = []
-        count = (len(test) + 1)
-        encorect = 1
 
-        for i in range(len(test)):
+        # Задаем временные переменные
+        list_hex_word = [] # Переменная нужна для перефоратирования предложения
+        count = (len(test) + 1) # Используется для высчитытования процента ошибки.
+        encorect = 1 # Счетчик ошибок
 
-            if (test[i] != test2[i]):
+        for i in range(len(test)): # Получаем количества слов
+            if (test[i] != test2[i]): # Проверяем, что слова не отличаются
                 print(test[i] + "\t" + test2[i] + "\t false")
 
-                if (100 / count * encorect) < procent_error:
-                    encorect = encorect + 1
-                    list_hex_word.append('*')
+                if (100 / count * encorect) < procent_error: # Проверяем процент отличия слов в предложение
+                    encorect = encorect + 1 # Если слова отличаются, то увиличиваем процент отличая сообщения
+                    list_hex_word.append('*') # Во временный массив добавляем вместо слова, которая отличается символ *
                 else:
-                    return
+                    return # Если процент отличая высокий, то остановить обработку и вернуть пустое значение
 
             else:
                 print(test[i] + "\t" + test2[i] + "\t true")
-                list_hex_word.append(poof.get_word_by_hex_word(test[i]))
+                list_hex_word.append(poof.get_word_by_hex_word(test[i])) # Во временную переменную добавить слова из объекта
 
-    return list_hex_word
+    return list_hex_word # Вернуть спсок слов
